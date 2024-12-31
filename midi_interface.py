27ec94a0944d9ae09cec405e76323eada16dbaeb
Reif Larsen <reifzlarsen@gmail.com>
2024-12-30 18:55:22 -0800

    Implemented MIDI queue, refresh method for midi clock.

--- midi_interface.py
+++ midi_interface.py
@@ -4,19 +4,17 @@ import pygame
 import math
 import keyboard
 
-dEBUGMODE = False
+dEBUGMODE = True
 
-
-#PYGAME DEFINITIONS:
-
-#define MIDI_EVENT
+#PYGAME EVENTS
 MIDI_EVENT = pygame.USEREVENT + 1
 
-def post_midi_event(pitch): #posts a MIDI_EVENT with a pitch attribute
-    event = pygame.event.Event(MIDI_EVENT, {"pitch": pitch})
+#PYGAME FUNCTIONS:
+def post_midi_event(pitch, vel): #posts a MIDI_EVENT with pitch and velocity
+    event = pygame.event.Event(MIDI_EVENT, {"pitch": pitch, "vel": vel})
     pygame.event.post(event)
 
-#GENERAL UTILITY FUNCTIONS:
+#UTILITIES:
 def get_note_str(input): #takes an integer MIDI pitch and returns a string in standard musical notation.
     notelist = ["C","C#/Db","D","D#/Eb","E","F","F#/Gb","G","G#/Ab","A","A#/Bb","B"]
     
@@ -31,7 +29,6 @@ def get_note_str(input): #takes an integer MIDI pitch and returns a string in st
     note = f"{pitch}{str(octave)}"
 
     return note
-
 def get_note(input): #takes int MIDI pitch and returns the note and octave as a 2-tuple of integers
         
     try: #checks for int
@@ -45,19 +42,28 @@ def get_note(input): #takes int MIDI pitch and returns the note and octave as a 
 
     return (note, octave)
 
-class MidiClock: #a class for the app
+#OBJECTS:
+class MidiClock: #a class for each clock
 
     def __init__(self):
-        self.midi_in = rtmidi.MidiIn() #creates a midi input class
+        
+        #configurations
         self.resolution = (800,800)
+        self.speed = 1000 #max lifespan for display objects in ms
+        
+        #structural variables
+        self.midi_in = rtmidi.MidiIn() #creates a midi input class
         self.origin = (self.resolution[0] / 2, self.resolution[1] / 2)
+        self.activemidi = []
+
+        #initialization calls
         self.OpenPorts()
 
     def OpenPorts(self):
         self.midi_in.open_port(0) #opens port 0 for midi input (assumes 0 is available)
         print(self.midi_in.get_ports()) #prints the port info - for testing
 
-    def DefaultDisplay(self): #initializes the default empty display
+    def ResetDisplay(self): #initializes the default empty display
         screen.fill((0,0,0))
         
         for note in range(12):
@@ -74,31 +80,50 @@ class MidiClock: #a class for the app
         #if there is an input, prints the details
         if msg:
             (ms, dt) = msg #the midi message
-            command = hex(ms[0]) #hexes first item, the midi command
-            pitch = int(ms[1]) #ms[1] is the pitch.  ms[2] is velocity.
+            command = hex(ms[0]) #hexes first item, the midi command, for interpretation
+            pitch = int(ms[1]) #ms[1] is pitch of midi signal 
+            vel = int(ms[2]) #ms[2] is velocity of midi signal
 
-            if command[2] == '9': #filters for note-on
+            if command[2] == '9': #interprets the midi command, filters for note-on
                 if dEBUGMODE: print(f"{command} {ms[1:]}\t| dt = {dt:.2f}")
-                print(get_note_str(pitch)) 
-                post_midi_event(pitch)
+                print(get_note_str(pitch))
+                post_midi_event(pitch, vel) #pygame posts a midi event
 
-    def DrawNote(self, pitch):      
+    def DrawNote(self, midiobject):      
         
-        try: #checks for int
-            intput = int(pitch)
-        except:
-            print("Error: Input must be an integer.")
-            return
-        
-        note = intput % 12
-        octave = int((intput - (intput % 12))/12)
-        color = (255,0,0)
+        if type(midiobject) == MidiObject: 
+            intput = midiobject.pitch
+            note = intput % 12
+            octave = int((intput - (intput % 12))/12)
+            color = (255,0,0)
 
-        angle = math.radians(90 - note * 30)
-        distance = octave * 50
-        x = int(self.origin[0] + distance * math.cos(angle))
-        y = int(self.origin[1] - distance * math.sin(angle))
-        pygame.draw.circle(screen, color, (x, y), 5)
+            angle = math.radians(90 - note * 30)
+            distance = octave * 50
+            x = int(self.origin[0] + distance * math.cos(angle))
+            y = int(self.origin[1] - distance * math.sin(angle))
+            pygame.draw.circle(screen, color, (x, y), 5)
+
+        else: print("Error: Expected MidiObject")
+
+    def AddMidi(self, pitch, vel):
+        self.activemidi.append(MidiObject(pitch, vel)) 
+
+    def Refresh(self): #refreshes the midi clock's queue of midi objects
+        for midiobject in self.activemidi:
+            if midiobject.Decay() == 0: #purges dead midi objects
+                self.activemidi.remove(midiobject)
+            else: self.DrawNote(midiobject) #draws the midi object
+
+class MidiObject:
+    def __init__(self, pitch, vel):
+        self.pitch = pitch
+        self.timeout = vel * 128 / midiclock.speed
+
+    def Decay(self):
+        if self.timeout >= 0:
+            self.timeout -= midiclock.interval
+            return self.timeout
+        return 0
 
 #/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
 #THE MAIN PART OF THE APPLICATION BELOW:|
@@ -118,7 +143,7 @@ while running:
     if keyboard.is_pressed("esc"): break
     if keyboard.is_pressed("space"): reset = True
     if reset: 
-        midiclock.DefaultDisplay()
+        midiclock.ResetDisplay()
         reset = False
 
     #sets up the default display
@@ -126,13 +151,16 @@ while running:
 
     #listens for inputs
     midiclock.Listen()
+    midiclock.Refresh()
 
     #cycles thru the pygame event queue with conditions
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         elif event.type == MIDI_EVENT:
-            midiclock.DrawNote(event.pitch)
+            #try: 
+            midiclock.AddMidi(MidiObject(event.pitch, event.vel))
+            #except: print("Oops!")
 
     pygame.display.flip()
     clock.tick(30)
